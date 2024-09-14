fe0563d38b0d3d7cd1ba0b620cd2e7d88927f119
Natasha <janeeyre@yandex.ru>
2024-09-14 14:22:25 +0300

    rename URL :wq :wq

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('test example.com', async ({ page }) => {
-  await page.goto('https://example.com');
+  await page.goto('https://google.com');
   const title = await page.title();
   expect(title).toBe('Example Domain');
 });
