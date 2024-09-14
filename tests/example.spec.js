8d9bbcf46d3a15287aad55f7e05f9d7e559b0ce3
ruacpad <alexey.p.cherepanov@raiffeisen.ru>
2024-09-14 15:32:05 +0300

    some changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('test example.com', async ({ page }) => {
-  await page.goto('https://example.com');
+  await page.goto('https://google.com');
   const title = await page.title();
   expect(title).toBe('Example Domain');
 });
+
+
+test('test-1', async ({ page }) => {
+  await page.goto("https://selectel.ru/blog/tutorials/what-is-git-push-and-how-to-use-it/");
+  let element = await page.locator('//h2[contains(@class, "wp-block-heading") and contains(@id, "Pushing-changes")]').textContent();
+  expect(element).toBe("Отправка изменений в чистый репозиторий");
+  });
